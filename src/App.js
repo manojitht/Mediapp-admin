61b8ad5a198146e55c8b5bc748ec4e98defe5c95
manojitht <manojith12345@outlook.com>
2021-03-19 14:37:16 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ function App() {
     <link rel="preconnect" href="https://fonts.gstatic.com"></link>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;900&display=swap" rel="stylesheet"></link>
     <body>
+      <div className="main">
+      <h3 className="mainfont">MediApp<br/>Create your admin account!</h3>
+      </div>
     <div className="section">
       <h1>Create admin account!</h1>
       <Register/>
     </div>
     </body>
+    {/* <footer>
+    <h5>Copyright © 2021 MediApp. All rights reserved.</h5>
+    </footer> */}
     </>
   );
 }
